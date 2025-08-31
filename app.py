0d49dc917355fd031f906feedaf79da966ba8eaf
Harsh-Kaithwas <kaithwasji.harsh@gmail.com>
2025-09-01 02:23:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="SocioGuard", layout="wide")
+st.title("SocioGuard – Social Media Impact Assessment (Prototype)")
+st.write("Setup successful. Coding will be added in next steps.")
