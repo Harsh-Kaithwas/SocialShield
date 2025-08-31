b60c8f5e33ff435f7a64b780c8c6f981700dcc30
Harsh-Kaithwas <kaithwasji.harsh@gmail.com>
2025-09-01 02:27:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.set_page_config(page_title="SocioGuard", layout="wide")
-st.title("SocioGuard – Social Media Impact Assessment (Prototype)")
+st.title("SocialShield – Social Media Impact Assessment (Prototype)")
 st.write("Setup successful. Coding will be added in next steps.")
